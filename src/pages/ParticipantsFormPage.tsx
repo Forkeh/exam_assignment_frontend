06a7595e2e5e1357664fa45a75abb37de19ba71e
Forkeh <forkeh@gmail.com>
2024-06-20 16:43:21 +0200

    form seems to be working, but checkboxes acting weird

--- src/pages/ParticipantsFormPage.tsx
+++ src/pages/ParticipantsFormPage.tsx
@@ -4,7 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { toast } from "@/components/ui/use-toast";
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
@@ -13,6 +13,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { IParticipant } from "@/models/IParticipant";
 import { getAllDisciplines } from "@/services/DisciplinesApi";
 import { IDiscipline } from "@/models/IDiscipline";
+import { Checkbox } from "@radix-ui/react-checkbox";
 
 const FormSchema = z.object({
     name: z.string().min(2, {
@@ -26,6 +27,9 @@ const FormSchema = z.object({
     }),
     club: z.string().min(1, {
         message: "Club must be at least 1.",
+    }),
+    disciplines: z.array(z.number()).refine((value) => value.some((item) => item), {
+        message: "You have to select at least one discipline.",
     }),
     // disciplines: z.array(z.number()).min(1, {
     //     message: "Must select at least 1 discipline.",
@@ -58,6 +62,7 @@ export default function ParticipantsFormPage() {
             gender: participant?.gender || "MALE",
             age: participant?.age || 0,
             club: participant?.club || "",
+            disciplines: disciplines.map((d) => d.id) || [],
         },
     });
 
@@ -158,19 +163,6 @@ export default function ParticipantsFormPage() {
                         </FormItem>
                     )}
                 />
-                {/* <FormField
-                    control={form.control}
-                    name="capacity"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Capacity</FormLabel>
-                            <FormControl>
-                                <Input placeholder="Capacity of the event" type="number" {...field} />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    )}
-                /> */}
 
                 <FormField
                     control={form.control}
@@ -200,6 +192,50 @@ export default function ParticipantsFormPage() {
                     )}
                 />
 
+                <FormField
+                    control={form.control}
+                    name="disciplines"
+                    render={() => (
+                        <FormItem>
+                            <div className="mb-4">
+                                <FormLabel className="text-base">Disciplines</FormLabel>
+                                <FormDescription>Select disciplines.</FormDescription>
+                            </div>
+                            {disciplines.map((item) => (
+                                <FormField
+                                    key={item.id}
+                                    control={form.control}
+                                    name="disciplines"
+                                    render={({ field }) => {
+                                        return (
+                                            <FormItem
+                                                key={item.id}
+                                                className="flex flex-row items-start space-x-3 space-y-0"
+                                            >
+                                                <FormControl>
+                                                    <Checkbox
+                                                        className="size-6 appearance-none w-4 h-4 border-2 border-blue-500 rounded-sm bg-white checked:bg-blue-800 checked:border-0"
+                                                        checked={field.value?.includes(item.id)}
+                                                        onCheckedChange={(checked) => {
+                                                            return checked
+                                                                ? field.onChange([...field.value, item.id])
+                                                                : field.onChange(
+                                                                      field.value?.filter((value) => value !== item.id)
+                                                                  );
+                                                        }}
+                                                    />
+                                                </FormControl>
+                                                <FormLabel className="font-normal">{item.name}</FormLabel>
+                                            </FormItem>
+                                        );
+                                    }}
+                                />
+                            ))}
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Checkbox>Checkbox</Checkbox>
                 <Button type="submit">{participant ? "Update" : "Create"}</Button>
             </form>
         </Form>
