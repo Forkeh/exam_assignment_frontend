a17cdd8c672d7035d33acace4152118df355a792
Forkeh <forkeh@gmail.com>
2024-06-20 15:20:15 +0200

    participant details dialog works

--- src/components/table/table-columns/ParticipantColumn.tsx
+++ src/components/table/table-columns/ParticipantColumn.tsx
@@ -1,7 +1,15 @@
 import { IParticipant } from "@/models/IParticipant";
 import { ColumnDef } from "@tanstack/react-table";
-import { FaRegEdit } from "react-icons/fa";
+import { FaRegEdit, FaInfoCircle } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
 
 export const ParticipantColumns: ColumnDef<IParticipant>[] = [
     {
@@ -32,9 +40,27 @@ export const ParticipantColumns: ColumnDef<IParticipant>[] = [
 
             return (
                 <div className="flex  hover:text-orange-500 transition-all">
-                    <Link to={"form"} state={participant}>
-                        <FaRegEdit size={22} />
-                    </Link>
+                    {/* <Link to={"form"} state={participant}>
+                        <FaInfoCircle size={22} />
+                    </Link> */}
+                    <Dialog>
+                        <DialogTrigger>
+                            <FaInfoCircle size={22} />
+                        </DialogTrigger>
+                        <DialogContent>
+                            <DialogHeader>
+                                <DialogTitle>{participant.name}</DialogTitle>
+                                <DialogDescription></DialogDescription>
+                            </DialogHeader>
+                            <div>
+                                <p>Id: {participant.id}</p>
+                                <p>Age: {participant.age}</p>
+                                <p>Gender: {participant.gender}</p>
+                                <p>Club: {participant.club}</p>
+                                <p>Disciplines: {participant.disciplines.join(", ")}</p>
+                            </div>
+                        </DialogContent>
+                    </Dialog>
                 </div>
             );
         },
