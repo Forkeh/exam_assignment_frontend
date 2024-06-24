7567ef5983d2bc514424b74fcb5fc47c7c92c315
Forkeh <forkeh@gmail.com>
2024-06-24 17:09:52 +0200

    added fade-in anim

--- src/pages/PageLayout.tsx
+++ src/pages/PageLayout.tsx
@@ -1,20 +1,31 @@
 import Header from "@/components/navbar/Header";
 import Navbar from "@/components/navbar/Navbar";
 import { Toaster } from "@/components/ui/toaster";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
 
 export default function PageLayout() {
-    return (
-        <main>
-            
-            <Header />
-            <Navbar />
-            <main className="flex justify-center ">
-                <div className="w-3/4">
-                    <Outlet />
-                </div>
-            </main>
-            <Toaster />
-        </main>
-    );
+	const location = useLocation();
+
+	return (
+		<main>
+			<Header />
+			<Navbar />
+			<main className="flex justify-center">
+				<motion.div
+					initial={{
+						opacity: 0,
+					}}
+					animate={{
+						opacity: 1,
+					}}
+					key={location.pathname}
+					className="w-3/4"
+				>
+					<Outlet />
+				</motion.div>
+			</main>
+			<Toaster />
+		</main>
+	);
 }
